746bc1ef193c360d15d35277498d6c4bb2fed8d5
Eric Maina <70095144+exuseric@users.noreply.github.com>
2022-09-02 19:03:15 +0300

    add scss global import

--- next.config.js
+++ next.config.js
@@ -1,7 +1,11 @@
 /** @type {import('next').NextConfig} */
+
 const nextConfig = {
   reactStrictMode: true,
   swcMinify: true,
+  sassOptions: {
+    additionalData: `@use './styles/config/partials/_index.scss' as *;`
+  }
 }
 
 module.exports = nextConfig
