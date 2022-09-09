acfe97978fbd1ef079acc338b87ba5c7841026dd
Eric Maina <70095144+exuseric@users.noreply.github.com>
2022-09-09 15:07:13 +0300

    update meta

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,23 @@
-import '../styles/globals.css'
+import Head from 'next/head';
+import '../styles/global.scss';
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return (
+    <>
+      <Head>
+        <title>atomella | Bold and Quirky</title>
+        <meta
+          name="description"
+          content="The place for beautifully designed garments that are bold with a smidge quirky."
+        />
+        <link
+          rel="icon"
+          href="/favicon.ico"
+        />
+      </Head>
+      <Component {...pageProps} />
+    </>
+  );
 }
 
-export default MyApp
+export default MyApp;
