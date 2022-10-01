7b34f9ff17844b39159a931b96dc02c1bf1b0a5f
Eric Maina <70095144+exuseric@users.noreply.github.com>
2022-10-01 19:41:20 +0300

    feat: verify domain

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,15 @@
 import '../styles/globals.css'
+import Head from 'next/head'
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return (
+    <div>
+      <Head>
+        <meta name="facebook-domain-verification" content="wp29qz9jcnkxtdqaw3jc1jhrzghvr6" />
+      </Head>
+      <Component {...pageProps} />
+    </div>
+  )
 }
 
 export default MyApp
